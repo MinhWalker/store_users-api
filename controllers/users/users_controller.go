01736b1e9e8a269dd068544b36f0b96009b7cee1
minhwalker <minhnh421999@gmail.com>
2021-02-23 22:22:56 +0700

    First add

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,30 @@
 package users
 
 import (
+	"github.com/MinhWalker/store_users-api/domain/users"
+	"github.com/MinhWalker/store_users-api/services"
+	"github.com/MinhWalker/store_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context)  {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		//TODO: return bad request to the caller
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		//TODO: handle user creation error
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context)  {
