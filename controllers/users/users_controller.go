9721c5aa1fe475b7ef389e1766fb7f723dde36b8
minhwalker <minhnh421999@gmail.com>
2021-02-24 17:47:31 +0700

    Create user api

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/MinhWalker/store_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context)  {
@@ -28,5 +29,18 @@ func CreateUser(c *gin.Context)  {
 }
 
 func GetUser(c *gin.Context)  {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		//TODO: handle user creation error
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
---
+++ domain/users/user_dto.go
@@ -0,0 +1,23 @@
+package users
+
+import (
+	"github.com/MinhWalker/store_users-api/utils/errors"
+	"strings"
+)
+
+type User struct {
+	Id          int64  `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+}
+
+//Method
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
+}
