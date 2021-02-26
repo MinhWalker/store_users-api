afeab7d16d64714418290e8200a43dab523e354d
minhwalker <minhnh421999@gmail.com>
2021-02-26 19:18:08 +0700

    add connectDB datasource

---
+++ datasources/mysql/users_db/users_db.go
@@ -0,0 +1,46 @@
+package users_db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+const(
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host = "mysql_users_host"
+	mysql_users_schema = "mysql_users_schema"
+)
+
+var(
+	Client *sql.DB
+
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host = os.Getenv(mysql_users_host)
+	schema = os.Getenv(mysql_users_schema)
+)
+
+func init()  {
+	datasourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4",
+		username, password, host, schema,
+		//"root",
+		//"1234",
+		//"127.0.0.1:3306",
+		//"users_db",
+		)
+	log.Println(fmt.Sprintf("about to connect to %s", datasourceName))
+	var err error
+	Client, err = sql.Open("mysql", datasourceName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = Client.Ping(); err != nil {
+		panic(err)
+	}
+	log.Println("database successfully configured")
+}
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/MinhWalker/store_users-api/datasources/mysql/users_db"
 	"github.com/MinhWalker/store_users-api/utils/date_utils"
 	"github.com/MinhWalker/store_users-api/utils/errors"
 )
@@ -22,6 +23,10 @@ func something()  {
 }
 
 func (user *User) Get() *errors.RestErr{
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("User %d not found", user.Id))
