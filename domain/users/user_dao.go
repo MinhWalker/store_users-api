dab904cce83b9aa07c654ded68ca0dff2b4757f2
minhwalker <minhnh421999@gmail.com>
2021-02-27 00:57:59 +0700

    done api get user

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -9,46 +9,33 @@ import (
 	"strings"
 )
 
-const(
+const (
 	indexUniqueEmail = "email_UNIQUE"
-	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	errorNoRows      = "no rows in result set"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?"
 )
 
-var(
-	usersDB = make(map[int64]*User)
-)
-
-func something()  {
-	user := User{}
-
-	if err := user.Get(); err != nil {
-		fmt.Println(err)
-		return
+func (user *User) Get() *errors.RestErr {
+	statement, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	fmt.Println(user.FirstName)
-}
+	defer statement.Close()
 
-func (user *User) Get() *errors.RestErr{
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	result := statement.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewInternalServerError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to get user %d: %s", user.Id, err.Error()))
 	}
-
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("User %d not found", user.Id))
-	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 
 	return nil
 }
 
 func (user *User) Save() *errors.RestErr {
 	statement, err := users_db.Client.Prepare(queryInsertUser)
-
 	if err != nil {
 		return errors.NewInternalServerError(err.Error())
 	}
@@ -56,7 +43,7 @@ func (user *User) Save() *errors.RestErr {
 
 	user.DateCreated = date_utils.GetNowString()
 
-	insertResult, err := statement.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	insertResult, err := statement.Exec(&user.FirstName, &user.LastName, &user.Email, &user.DateCreated)
 	if err != nil {
 		if strings.Contains(err.Error(), indexUniqueEmail) {
 			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists!", user.Email))
