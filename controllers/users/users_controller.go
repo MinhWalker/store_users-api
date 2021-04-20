9c4b4b00bcfdac274b42d258f925a606563cf25e
minhwalker <minhnh421999@gmail.com>
2021-04-20 16:52:51 +0700

    modify util

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -38,7 +38,7 @@ func Create(c *gin.Context) {
 func Get(c *gin.Context) {
 	// TODO: authenticate request
 	if err := oauth.AuthenticateRequest(c.Request); err != nil {
-		c.JSON(err.Status, err)
+		c.JSON(err.Status(), err)
 		return
 	}
 
